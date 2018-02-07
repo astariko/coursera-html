e529dfbbdc3c436117ab4739714dfbb9452df521
Artem Starikov <Starikov.Artem@gmail.com>
2018-02-06 23:31:12 -0500

    ass 3

---
+++ assignment3/app.js
@@ -0,0 +1,83 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', foundItemsDirective);
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var search = this;
+  search.items = []; //== found
+  search.searchItem = "";
+
+  search.search = function (){
+    search.errorMessage = "";
+    if (search.searchItem =="") {
+      search.errorMessage = "Please enter a search criteria"
+      return
+    }
+
+    var promise = MenuSearchService.getMatchedMenuItems(search.searchTerm);
+
+    promise.then(function (response) {
+      var items = response.data.menu_items;
+      //console.log(search.items)
+
+      search.items = []
+      for (var i=0; i < items.length; i++) {
+        if (items[i].description.indexOf(search.searchItem) >= 0){
+          search.items.push(items[i]);
+        }
+      }
+
+      if (search.items.length == 0){
+        search.errorMessage = "Nothing found."
+      }
+    })
+    .catch(function (error) {
+      search.errorMessage = "Something went terribly wrong."
+    });
+  }
+
+
+  search.removeItem = function (index) {
+    search.items.splice(index, 1);
+  };
+
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    })
+
+    return response;
+  };
+}
+
+foundItemsDirective.$inject = []
+function foundItemsDirective() {
+	var ddo = {
+		templateUrl: 'found.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+
+    controller: NarrowItDownController,
+    bindToController: true,
+    controllerAs: 'search'
+	};
+	return ddo;
+}
+
+})();
